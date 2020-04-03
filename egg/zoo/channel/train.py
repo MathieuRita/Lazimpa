8c2a3ba869b52c32c989c9a8fb59f4cd8c330210
Mathieu <mathieu.rita@polytechnique.edu>
2020-04-03 13:03:03 +0200

    init

--- egg/zoo/channel/train.py
+++ egg/zoo/channel/train.py
@@ -166,11 +166,12 @@ def main(params):
     trainer = core.Trainer(game=game, optimizer=optimizer, train_data=train_loader,
                            validation_data=test_loader, callbacks=[EarlyStopperAccuracy(opts.early_stopping_thr)])
 
-    trainer.train(n_epochs=opts.n_epochs)
-    if opts.checkpoint_dir:
-        trainer.save_checkpoint(name=f'{opts.name}_vocab{opts.vocab_size}_rs{opts.random_seed}_lr{opts.lr}_shid{opts.sender_hidden}_rhid{opts.receiver_hidden}_sentr{opts.sender_entropy_coeff}_reg{opts.length_cost}_max_len{opts.max_len}')
+    for epoch in opts.n_epochs:
+        trainer.train(n_epochs=1)
+        if opts.checkpoint_dir:
+            trainer.save_checkpoint(name=f'{opts.name}_vocab{opts.vocab_size}_rs{opts.random_seed}_lr{opts.lr}_shid{opts.sender_hidden}_rhid{opts.receiver_hidden}_sentr{opts.sender_entropy_coeff}_reg{opts.length_cost}_max_len{opts.max_len}')
 
-    dump(trainer.game, opts.n_features, device, False)
+        dump(trainer.game, opts.n_features, device, False)
     core.close()
 
 

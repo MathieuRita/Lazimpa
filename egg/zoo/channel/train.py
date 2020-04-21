ed9f8097e286eb4e8dd06250dac8089ec19d912f
Mathieu <mathieu.rita@polytechnique.edu>
2020-04-21 13:49:58 +0200

    impatient listener

--- egg/zoo/channel/train.py
+++ egg/zoo/channel/train.py
@@ -110,15 +110,9 @@ def dump(game, n_features, device, gs_mode):
 
         acc_vec[int(input_symbol)]=acc
 
-        # Acc by class
-        #acc1 = (input_symbol[:25] == output_symbol[:25]).float().item()
-        #acc2 = (input_symbol[25:50] == output_symbol[25:50]).float().item()
-        #acc3 = (input_symbol[50:75] == output_symbol[50:75]).float().item()
-        #acc4 = (input_symbol[75:100] == output_symbol[75:100]).float().item()
-
         unif_acc += acc
         powerlaw_acc += powerlaw_probs[input_symbol] * acc
-        #print(f'input: {input_symbol.item()} -> message: {",".join([str(x.item()) for x in message])} -> output: {output_symbol.item()}', flush=True)
+        print(f'input: {input_symbol.item()} -> message: {",".join([str(x.item()) for x in message])} -> output: {output_symbol.item()}', flush=True)
 
     unif_acc /= n_features
 

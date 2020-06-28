ac3a5dbced2149c0c0284e541bc19ccdfdeba96e
Mathieu <mathieu.rita@polytechnique.edu>
2020-06-28 23:15:07 +0200

    probs attributes

--- egg/core/reinforce_wrappers.py
+++ egg/core/reinforce_wrappers.py
@@ -1094,6 +1094,7 @@ class CompositionalitySenderImpatientReceiverRnnReinforce(nn.Module):
 
     def forward(self, sender_input, labels, receiver_input=None):
 
+        print(sender_input)
         message, log_prob_s, entropy_s = self.sender(torch.floor(sender_input))
         message_lengths = find_lengths(message)
 

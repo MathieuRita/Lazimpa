3d4832bec7718cb0a82b57185244e09682aa3003
Mathieu <mathieu.rita@polytechnique.edu>
2020-06-29 16:35:53 +0200

    loss change

--- egg/zoo/channel/train_compositionality.py
+++ egg/zoo/channel/train_compositionality.py
@@ -454,7 +454,7 @@ def main(params):
         new_acc=np.mean(acc_vec)
         if epoch%5==0:
           if np.abs(new_acc-np.mean(curr_accs))<0.01:
-            trainer.optimizer.defaults["lr"]/=10
+            trainer.optimizer.defaults["lr"]/=4
           print(trainer.optimizer.defaults["lr"])
         curr_accs[epoch%7]=new_acc
 

959018730a95d3bea2230c9b25f98088b100b857
Mathieu <mathieu.rita@polytechnique.edu>
2020-06-29 22:43:56 +0200

    loss change

--- egg/zoo/channel/train_compositionality.py
+++ egg/zoo/channel/train_compositionality.py
@@ -377,8 +377,8 @@ def main(params):
     if opts.probs_attributes=="echelon":
         probs_attributes=[]
         for i in range(opts.n_attributes):
-            probs_attributes.append(1.-(0.2)*i)
-            #probs_attributes.append(0.6+0.4/(i+1))
+            #probs_attributes.append(1.-(0.2)*i)
+            probs_attributes.append(0.7+0.3/(i+1))
 
     print("Probability by attribute is:",probs_attributes)
 

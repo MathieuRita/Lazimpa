7c3ea527edb0eb9eb6e69ba4120325ecef1ff3ee
Mathieu <mathieu.rita@polytechnique.edu>
2020-08-07 12:15:09 +0200

    reg imp

--- egg/zoo/channel/train_compositionality.py
+++ egg/zoo/channel/train_compositionality.py
@@ -7,6 +7,7 @@ import json
 import argparse
 import numpy as np
 import itertools
+import torch
 import torch.utils.data
 import torch.nn.functional as F
 from torch.optim.lr_scheduler import ReduceLROnPlateau

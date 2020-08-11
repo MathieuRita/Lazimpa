7bc941b10808771bad099bb54890cc2531788de9
Mathieu <mathieu.rita@polytechnique.edu>
2020-08-11 10:42:10 +0200

    print compo

--- egg/zoo/channel/features.py
+++ egg/zoo/channel/features.py
@@ -149,10 +149,25 @@ class _OneHotIteratorCompositionality:
             #batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
 
             # Ici on implemente le fait qu un attribut peut ne pas etre sample
-            if np.random.rand()<self.probs_attributes[i]:
+            #if np.random.rand()<self.probs_attributes[i]:
+            #    batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+            #else:
+            #    batch_data_att.append(0*self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+
+            # On gère les couples
+            rd=np.random.rand()
+            i=0
+
+            if rd<0.2:
                 batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
-            else:
+                batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+            if rd>0.2 and rd<0.6:
+                batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
                 batch_data_att.append(0*self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+            if rd>0.6:
+                batch_data_att.append(0*self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+                batch_data_att.append(self.random_state.multinomial(1, self.probs[i], size=self.batch_size).astype(float))
+
 
 
         batch_data=batch_data_att[0]
